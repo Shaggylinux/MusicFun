a13141e9abe2812356a0f206f89357f2c2818b16
Shaggy <malacruz132@gmail.com>
2026-04-17 15:50:19 -0500

    Fix : Download configuration

--- src/main.py
+++ src/main.py
@@ -1,60 +1,57 @@
 import flet   as ft
 import yt_dlp as yt
-import platform
 import threading
 
 def main(page : ft.Page):
-    def videoname(url):
+    def videoinfo(url):
         ydl_opts = {
             'quiet': True,
             'skip_download': True
         }
         with yt.YoutubeDL(ydl_opts) as ydl:
             info      = ydl.extract_info(url, download = False)
-            title     = info.get('title', 'No encontrado')
-            miniatura = info.get('thumbnail', 'No encontrado')
-            duracion = info.get('duration', 0)
+            title     = info.get('title', 'No found')
+            miniatura = info.get('thumbnail', 'No found')
+            duracion  = info.get('duration', 0)
             return title, miniatura, duracion
-    
-    def videdownloader():
-        url = entry.value
-        ruta = ""
-        if page.platform == ft.PagePlatform.ANDROID:
-            ruta = "/storage/emulated/0/Download/"
-        else:
-            ruta = "Downloads/"
-        ydl_opts = {
-                    'format': 'bestaudio/best',
-                    'outtmpl': f'{ruta}%(title)s.%(ext)s',
-                    'fixup': 'never',
-                    'nopart': True,
-                    'prefer_ffmpeg': False,
-                    'external_downloader': None
-                }
-        with yt.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        threading.Thread(target=videdownloader, daemon=True).start()
 
-    def search(e):
+    def viewdownload():
+        url = entry.value
+        def rundownload():
+            ruta = ""
+            if page.platform == ft.PagePlatform.ANDROID:
+                ruta = "/storage/emulated/0/Download/"
+            else:
+                ruta = "Downloads/"
+            ydl_opts = {
+                        'format': 'bestaudio/best',
+                        'outtmpl': f'{ruta}%(title)s.%(ext)s',
+                        'fixup': 'never',
+                        'nopart': True,
+                        'prefer_ffmpeg': False,
+                        'external_downloader': None
+                    }
+            with yt.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+        threading.Thread(target = rundownload, daemon = True).start()
+
+    def search():
         if (entry.value == ""):
             DownloadText.value = "Title : Error"
         else:
-            nombre, miniatura, duracion  = videoname(entry.value)
-            Image.src          = f"{miniatura}"
-            DownloadText.value = f"Title : {nombre}"
-            DurationText.value = f"Duration : {duracion // 60}:{duracion % 60}"
-            DownloadButton.content  = "Download"
-            DownloadButton.on_click = videdownloader
-            DownloadButton.update()
-            Image.update()
-            DownloadText.update()
-            DurationText.update()
+            nombre, miniatura, duracion  = videoinfo(entry.value)
+            Image.src                    = f"{miniatura}"
+            DownloadText.value           = f"Title : {nombre}"
+            DurationText.value           = f"Duration : {duracion // 60}:{duracion % 60}"
+            DownloadButton.content       = "Download"
+            DownloadButton.on_click      = viewdownload
+            page.update()
 
     entry          = ft.TextField(multiline = True)
     DownloadText   = ft.Text("Title : None")
     Image          = ft.Image(src = "NoImage.png", width = 256, height = 256)
     DownloadButton = ft.Button("Search", on_click = search)
-    DurationText   = ft.Text("Duration : 0:00")
+    DurationText   = ft.Text("Duration : --:--")
     
     page.controls.append(Image)
     page.controls.append(entry)
